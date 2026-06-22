98f5ad90e5a7616b721a803244215d09b83ff36e
Eph23 <ephraims.818@gmail.com>
2026-06-23 05:34:27 +0600

    REACT--ULTIMATE-COURSE--REV - Components, Props and JSX - 12. Setting Classes and Text Coditionally

--- 01. components, props, jsx/pizza-menu/src/App.jsx
+++ 01. components, props, jsx/pizza-menu/src/App.jsx
@@ -9,17 +9,15 @@ function Header() {
 }
 
 function Pizza({ pizzaObj }) {
-    if (pizzaObj.soldOut) {
-        return null;
-    }
-
     return (
-        <li className="pizza">
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
             <img src={pizzaObj.photoName} alt={pizzaObj.name} />
             <div>
                 <h3>{pizzaObj.name}</h3>
                 <p>{pizzaObj.ingredients}</p>
-                <span>{pizzaObj.price + 3}</span>
+                <span>
+                    {pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price + 3}
+                </span>
             </div>
         </li>
     );
@@ -27,7 +25,7 @@ function Pizza({ pizzaObj }) {
 
 function Menu() {
     const pizzas = pizzaData;
-    const numPizzas = 0;
+    const numPizzas = pizzas.length;
 
     return (
         <main className="menu">
