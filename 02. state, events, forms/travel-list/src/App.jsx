d01ddd5ebfca16e65f6d859d5cc9837718450554
Eph23 <ephraims.818@gmail.com>
2026-06-30 21:20:49 +0600

    REACT--ULTIMATE-COURSE--REV - State, Events and Forms - PROJECT -- Travel List - 02. Rendering the Items List

--- 02. state, events, forms/travel-list/src/App.jsx
+++ 02. state, events, forms/travel-list/src/App.jsx
@@ -1,3 +1,9 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: false },
+    { id: 2, description: "Charger", quantity: 2, packed: true },
+    { id: 2, description: "T-Shirts", quantity: 5, packed: true },
+];
 
 function Logo() {
     return (
@@ -14,9 +20,30 @@ function Form() {
         </div>
     );
 }
-function PackingList() {
-    return <div className="list">LIST</div>;
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>❌</button>
+        </li>
+    );
 }
+
+function PackingList() {
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
 function Status() {
     return (
         <footer className="stats">
